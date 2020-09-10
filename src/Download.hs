6fd249a4dfb60d5b40afea1b6fcc3a4102c7f6a2
Grace Rarer <gracerarer@gmail.com>
2020-09-10 15:10:31 -0400

    determine file extension from image file content
    we now use Image-type to determine the appropriate image file extension
    based on the content of the image file itself, and attempting to
    extract the extension from the URL is now a fall-back.
    
    I use nprindle's fork of Image-type, which provides for identifying
    image type from ByteStrings, since the upstream version of Image-type
    currently only supports identifying image type for files.

--- src/Download.hs
+++ src/Download.hs
@@ -12,9 +12,12 @@ import System.Directory ( createDirectoryIfMissing )
 import Fetch ( getRelative )
 import Text.HTML.TagSoup (parseTags, Tag)
 import Text.URI (render, mkURI)
-import Data.Maybe (listToMaybe)
+import Data.Maybe (fromJust, listToMaybe)
 import Data.Foldable (traverse_)
 import System.FilePath ((<.>), (</>))
+import Safe (lastMay)
+import Codec.ImageType (getImageType)
+import Control.Applicative((<|>))
 
 downloadPage :: ComicPage -> [ImageMeta] -> IO ()
 downloadPage page [singlePanel] = downloadPanel page Nothing singlePanel
@@ -29,16 +32,20 @@ downloadPanel page panelNumber imageMeta = do
         Nothing -> putStrLn "Error: failed to fetch image"
         Just imageResponse -> do
             createDirectoryIfMissing True outputDirectory
-            let fileName = imageFileName imageMeta page panelNumber
+            let contents = responseBody imageResponse
+            let fileName = imageFileName imageMeta contents page panelNumber
             BS.writeFile fileName $ responseBody imageResponse
 
 outputDirectory :: FilePath
 outputDirectory = "comics-download"
 
-imageFileName :: ImageMeta -> ComicPage -> Maybe Int -> FilePath
-imageFileName image page panelNumber = outputDirectory </> name <.> extension
+imageFileName :: ImageMeta -> BS.ByteString -> ComicPage -> Maybe Int -> FilePath
+imageFileName metadata fileContents page panelNumber = outputDirectory </> name <.> extension
     where
         name = case panelNumber of
             Nothing -> filePrefix page ++ show (pageNumber page)
-            Just panelNumber' -> filePrefix page ++ show (pageNumber page) ++ "_" ++ show panelNumber
-        extension = Text.unpack $ last $ Text.splitOn "." $ imageSrc image
+            Just panelNumber' -> filePrefix page ++ show (pageNumber page) ++ "_" ++ (show panelNumber')
+        extensionFromUrl url = do
+            end <- lastMay $ Text.splitOn "/" url
+            fmap Text.unpack $ lastMay $ Text.splitOn "." url
+        extension = fromJust $ getImageType fileContents <|> (extensionFromUrl $ imageSrc metadata) <|> Just "UNKNOWN"
